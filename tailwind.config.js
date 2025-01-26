a4b811cfb95d644e047e603cf9a1a400050f7b45
ywgi <ywgi@outlook.com>
2025-01-26 01:56:25 -0600

    Add images and update configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,27 @@ export default {
       fontFamily: {
         barlow: ['Barlow', 'sans-serif'],
         lexend: ['Lexend', 'sans-serif'],
-        handwriting: ['"Oooh Baby"', 'cursive'],
-        handwriting2: ["Montez", 'serif']
+        handwriting: ['Oooh Baby', 'cursive'],
+        handwriting2: ['Montez', 'serif'],
+        quote: ['Sour Gummy', 'serif']
       },
       colors: {
         'green-custom': '#6f9558',
         'teal-custom': '#315851',
         'coral-custom': '#c17557',
         'gold-custom': '#e6a501',
-        'light-pink-custom': '#f2e8e6'
+        'light-pink-custom': '#f2e8e6',
+        'purple-custom': '#805994'
       },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' }
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in .5s ease-out forwards'
+      }
     },
   },
 }
